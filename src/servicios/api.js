813c23e71ce3bf7b28766d4b93c4a1a28062eeb6
MartinIgnaci0 <mar.pizarroe@duocuc.cl>
2026-05-13 09:58:27 -0400

    Cambie el api port de 3000 a 8080 xD se me pasó antes

--- src/servicios/api.js
+++ src/servicios/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Configuración base de axios
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
